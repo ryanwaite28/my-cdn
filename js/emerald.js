06bd1410b8a9e2be7e8a09d3dcc81912c9c5c97d
Ryan M. Waite <ryanwaite28@gmail.com>
2017-11-29 09:26:28 -0500

    Update emerald.js

--- js/emerald.js
+++ js/emerald.js
@@ -55,6 +55,15 @@ function getUrlParameter(name) {
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == variable){return pair[1];}
+	}
+	return false;
+}
 
 Date.prototype.addDays = function (number_of_days) {
    var dat = new Date(this.valueOf())
