28925ae2197e73c736e675a0b54ea0ae4a17f3bb
Ryan M. Waite <ryanwaite28@gmail.com>
2017-11-28 22:25:29 -0500

    Update emerald.js

--- js/emerald.js
+++ js/emerald.js
@@ -20,6 +20,20 @@ $(document).ready(function(){
 	
 })
 
+var parseQueryString = function() {
+
+    var str = window.location.search;
+    var objURL = {};
+
+    str.replace(
+        new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+        function( $0, $1, $2, $3 ){
+            objURL[ $1 ] = $3;
+        }
+    );
+    return objURL;
+};
+
 Date.prototype.addDays = function (number_of_days) {
    var dat = new Date(this.valueOf())
    dat.setDate(dat.getDate() + number_of_days);
