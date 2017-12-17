feb5fac33430d33c6828899ca2389b01d87f71fb
Ryan M. Waite <ryanwaite28@gmail.com>
2017-12-17 16:27:36 -0500

    Update emerald.js

--- js/emerald.js
+++ js/emerald.js
@@ -189,6 +189,13 @@ function randomValueLong(){
   return Math.random().toString(36).substr(2, 34) + Math.random().toString(36).substr(2, 34);
 }
 
+function uniqueValue() {
+    return String(Date.now()) + 
+	    Math.random().toString(36).substr(2, 34) +
+	    Math.random().toString(36).substr(2, 34) +
+	    Math.random().toString(36).substr(2, 34)
+}
+
 function string_insert(str, index, value) {
     return str.substr(0, index) + value + str.substr(index);
 }
