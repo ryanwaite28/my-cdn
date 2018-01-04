162cfde13992f4db4af0f77434f61285c4eb6b67
Ryan M. Waite <ryanwaite28@gmail.com>
2018-01-04 10:38:59 -0500

    Update emerald.js

--- js/emerald.js
+++ js/emerald.js
@@ -251,3 +251,13 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
 }
+
+// Angular interpolation
+/*
+
+App.config(['$interpolateProvider', function($interpolateProvider) {
+	$interpolateProvider.startSymbol('((');
+	$interpolateProvider.endSymbol('))');
+}]);
+
+*/
